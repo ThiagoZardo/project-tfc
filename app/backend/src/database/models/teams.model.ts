5cfbcee24b852c9c7e743772ffca4fbfb4ae4ceb
ThiagoZardo <thiago.artevisual@gmail.com>
2022-08-19 04:40:33 -0300

    Criei model Teams

---
+++ app/backend/src/database/models/teams.model.ts
@@ -0,0 +1,25 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id: number;
+  teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Teams;
